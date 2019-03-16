5ae4b075e76f808153a05bae42ff1973d990af23
rudtyz <rudtyz@gmail.com>
2019-03-16 10:37:12 +0900

    mute device

---
+++ mute.cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+#include <mmdeviceapi.h>
+#include <functiondiscoverykeys_devpkey.h>
+#include <Windows.h>
+#include <endpointvolume.h>
+#define SAFE_RELEASE(punk)  \
+              if ((punk) != NULL)  \
+                { (punk)->Release(); (punk) = NULL; }
+
+class CMMNotificationClient : public IMMNotificationClient
+{
+	
+	IAudioEndpointVolume* defaultAudioVolume = nullptr;
+
+public:
+	CMMNotificationClient() { }
+
+	void attachVolumeNotification() {
+		/**
+		 * 디바이스로부터 기본 volume조절기를 가져와서 등록
+		 * (defaultAudioVolume)
+		 * 나머지는 해제
+		 */
+		IMMDeviceEnumerator *deviceEnumerator = nullptr;
+		const CLSID CLSID_MMDeviceEnumerator = __uuidof(MMDeviceEnumerator);
+		const IID IID_IMMDeviceEnumerator = __uuidof(IMMDeviceEnumerator);
+		auto result = CoCreateInstance(
+			CLSID_MMDeviceEnumerator, NULL,
+			CLSCTX_ALL, IID_IMMDeviceEnumerator,
+			(void**)&deviceEnumerator);
+
+		deviceEnumerator->RegisterEndpointNotificationCallback(this);
+
+		IMMDevice* device = nullptr;
+		deviceEnumerator->GetDefaultAudioEndpoint(eRender, eMultimedia, &device);
+
+		device->Activate(__uuidof(IAudioEndpointVolume), CLSCTX_ALL, nullptr, (void**)&defaultAudioVolume);
+		SAFE_RELEASE(device);
+		SAFE_RELEASE(deviceEnumerator);
+	}
+
+	HRESULT STDMETHODCALLTYPE OnDeviceStateChanged(LPCWSTR pwstrDeviceId,DWORD dwNewState){
+		/**
+		 * 여기서 실제적으로 mute를 설정.
+		 * device가 
+		 */
+		switch (dwNewState)
+		{
+		case DEVICE_STATE_ACTIVE:
+			break;
+		case DEVICE_STATE_UNPLUGGED:
+			defaultAudioVolume->SetMute(true, nullptr);
+			break;
+		}
+		return S_OK;
+	}
+
+	~CMMNotificationClient() {
+		SAFE_RELEASE(defaultAudioVolume);
+	}
+
+	/**
+	 * 이 아래부터는 COM 인터페이스라서 건들필요 없음
+	 */
+	ULONG STDMETHODCALLTYPE AddRef() { return 1; }
+
+	ULONG STDMETHODCALLTYPE Release(){ return 1; }
+
+	HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid, VOID **ppvInterface){
+		return S_OK;
+	}
+	
+	HRESULT STDMETHODCALLTYPE OnDefaultDeviceChanged(EDataFlow flow, ERole role, LPCWSTR pwstrDeviceId){
+		return S_OK;
+	}
+
+	HRESULT STDMETHODCALLTYPE OnDeviceAdded(LPCWSTR pwstrDeviceId) {
+		return S_OK;
+	};
+
+	HRESULT STDMETHODCALLTYPE OnDeviceRemoved(LPCWSTR pwstrDeviceId) {
+		return S_OK;
+	}
+
+	HRESULT STDMETHODCALLTYPE OnPropertyValueChanged(LPCWSTR pwstrDeviceId,	const PROPERTYKEY key){
+		return S_OK;
+	}
+};
+//int main()
+//{
+//	
+//	CMMNotificationClient c;
+//	getchar(); 
+//}
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+	CoInitializeEx(NULL, COINIT_MULTITHREADED);
+	CMMNotificationClient c;
+	c.attachVolumeNotification();
+
+
+	//좀비 프로세스처럼 동작할거라서 
+	//계속 꺼지지않아도 상관없음
+	MSG msg;
+	while (GetMessage(&msg, 0, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return 0; 
+}
